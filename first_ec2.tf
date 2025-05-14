e56ad6f39ed66f9824b0adfac235c79696d4b621
JALASHREE11 <67895756+JALASHREE11@users.noreply.github.com>
2025-05-14 12:37:17 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "demoEC2" {
-  ami           = "ami-0b6d6dacf350ebc82"
+  ami           = "ami-04999cd8f2624f834"
   instance_type = "t2.micro"
   tags = {
     Name = "Demo EC2"
